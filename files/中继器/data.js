b8f79f70a4f439ec215e4f647fff3cc418ace539
jnn <1076871350@qq.com>
2020-04-21 13:53:52 +0800

    first

---
+++ files/中继器/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,_(g,h,i,_(j,k,l,m)),n,[],o,_(h,p),q,[r,s],t,_(u,v,w,x,g,y,z,_(),A,[],B,_(C,D,E,F,G,_(H,I,J,K),L,null,M,F,F,N,O,P,null,Q,R,S,T,U,V,W,R,X,Y,_(H,I,J,Z),ba,R,bb,bc,_(bd,be,bf,bg,bh,bg,bi,bg,J,_(bj,bk,bl,bk,bm,bk,bn,bo)),i,_(j,k,l,m)),bp,_(),bq,_(),br,_(bs,[])),bt,_(),bu,_());}; 
+var b="url",c="中继器.html",d="generationDate",e=new Date(1587446375609.48),f="defaultAdaptiveView",g="name",h="",i="size",j="width",k=375,l="height",m=667,n="adaptiveViews",o="sketchKeys",p="s0",q="variables",r="keyboardState",s="textLen",t="page",u="packageId",v="ff308c2edbc24c088110c5bce3050446",w="type",x="Axure:Page",y="中继器",z="notes",A="annotations",B="style",C="baseStyle",D="627587b6038d43cca051c114ac41ad32",E="pageAlignment",F="near",G="fill",H="fillType",I="solid",J="color",K=0xFFFFFFFF,L="image",M="imageAlignment",N="imageRepeat",O="auto",P="favicon",Q="sketchFactor",R="0",S="colorStyle",T="appliedColor",U="fontName",V="Applied Font",W="borderWidth",X="borderVisibility",Y="borderFill",Z=0xFF797979,ba="cornerRadius",bb="cornerVisibility",bc="outerShadow",bd="on",be=false,bf="offsetX",bg=5,bh="offsetY",bi="blurRadius",bj="r",bk=0,bl="g",bm="b",bn="a",bo=0.349019607843137,bp="adaptiveStyles",bq="interactionMap",br="diagram",bs="objects",bt="masters",bu="objectPaths";
+return _creator();
+})());
